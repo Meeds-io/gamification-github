07d0655f54050136afc7e5de93b587741246b8e5
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-12-15 13:32:05 +0100

    feat: Engagement Center Access to automatic/manuel rules detail - MEED-1262 - Meeds-io/MIPs#13 #254 (#63)
    Allow users to access rule detail via a drawer in the program detail

--- gamification-github-webapp/src/main/webapp/vue-app/engagementCenterExtensions/initComponents.js
+++
@@ -1,9 +0,0 @@
-import GithubActionValue from './components/GithubActionValue.vue';
-const components = {
-  'github-action-value': GithubActionValue,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
-
