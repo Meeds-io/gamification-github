c7d5cad439136aa69442ddd93744d0b528e52ef3
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-09-18 12:02:12 +0200

    feat: Remove old github connector portlet - MEED-2274 - Meeds-io/MIPs#64 (#101)

--- gamification-github-webapp/src/main/webapp/vue-app/webhook-management/initComponents.js
+++
@@ -1,9 +0,0 @@
-import GitHubWebHookManagementApp from './components/GitHubWebHookManagementApp.vue';
-
-const components = {
-  'github-webhook-management-app': GitHubWebHookManagementApp,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
