7930f255017bf791ed931abbcf64412eee81049e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-12-23 05:42:18 +0100

    feat: Cleanup Dependencies to rely on Spring Dependencies Tree - Meeds-io/MIPs#57

--- gamification-github-services/src/main/java/org/exoplatform/gamification/github/entity/WebhookEntity.java
+++ gamification-github-services/src/main/java/org/exoplatform/gamification/github/entity/WebhookEntity.java
@@ -19,7 +19,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 import org.exoplatform.commons.api.persistence.ExoEntity;
 
