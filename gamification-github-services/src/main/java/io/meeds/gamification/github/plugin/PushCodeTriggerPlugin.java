2c76d4e6da285d60dad55a4882660849bea7e299
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-02-23 17:07:19 +0100

    feat: Implement UT for github service - MEED-3381 - Meeds-io/MIPs#105 (#160)

---
+++ gamification-github-services/src/main/java/io/meeds/gamification/github/plugin/PushCodeTriggerPlugin.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.gamification.github.plugin;
+
+import io.meeds.gamification.github.model.Event;
+
+import java.util.*;
+
+import static io.meeds.gamification.github.utils.Utils.*;
+
+public class PushCodeTriggerPlugin extends GithubTriggerPlugin {
+
+  @Override
+  public List<Event> getEvents(Map<String, Object> payload) {
+    return Collections.singletonList(new Event(PUSH_CODE_EVENT_NAME,
+                                               null,
+                                               extractSubItem(payload, PUSHER, NAME),
+                                               extractSubItem(payload, HEAD_COMMIT, URL),
+                                               null,
+                                               extractSubItem(payload, ORGANIZATION, ID),
+                                               extractSubItem(payload, REPOSITORY, ID)));
+  }
+}
